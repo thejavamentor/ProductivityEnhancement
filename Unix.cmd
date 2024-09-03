358c565177f5703288c2c744b6b5f5b10874c72b
thejavamentor <thejavamentor@gmail.com>
2024-09-03 12:20:58 +0530

    Unix command used day to day besis.

---
+++ Unix.cmd
@@ -0,0 +1,21 @@
+export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
+
+export PATH="/opt/homebrew/bin/mvn:$PATH"
+
+# This is to find a directory 'SoH' in root directory 
+find / -type d  -name SoH
+
+
+find / -type d  -name snyk
+
+Pycharm
+
+
+
+## Change permission recursively
+chmod -R 777 *
+
+
+## copy file recursively | cp_recursively
+cp -Rf soh-comparison/* ../in45843881/soh-comparison/
+
